93a454754d5c1a8033cf4c405c1e8354c92d4839
Nayab Naeem <nayabnaeem666@gmail.com>
2026-07-18 02:56:11 +0500

    Fix Axios general.js for final task

--- final_project/general.js
+++ final_project/general.js
@@ -1,57 +1,28 @@
 const axios = require("axios");
 
-// Task 10.1 - Get all books (Async/Await)
+const BASE_URL = "http://localhost:5000";
+
 async function getAllBooks() {
-    try {
-        const response = await axios.get("http://localhost:5000/");
-        console.log("All Books:");
-        console.log(response.data);
-    } catch (error) {
-        console.log(error.message);
-    }
+    const response = await axios.get(`${BASE_URL}/`);
+    console.log(response.data);
 }
 
-// Task 10.2 - Get book by ISBN (Promise)
-function getBookByISBN(isbn) {
-    axios
-        .get(`http://localhost:5000/isbn/${isbn}`)
-        .then(response => {
-            console.log("Book by ISBN:");
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.log(error.message);
-        });
+async function getBookByISBN(isbn) {
+    const response = await axios.get(`${BASE_URL}/isbn/${isbn}`);
+    console.log(response.data);
 }
 
-// Task 10.3 - Get books by Author (Promise)
-function getBooksByAuthor(author) {
-    axios
-        .get(`http://localhost:5000/author/${author}`)
-        .then(response => {
-            console.log("Books by Author:");
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.log(error.message);
-        });
+async function getBookByAuthor(author) {
+    const response = await axios.get(`${BASE_URL}/author/${author}`);
+    console.log(response.data);
 }
 
-// Task 10.4 - Get books by Title (Promise)
-function getBooksByTitle(title) {
-    axios
-        .get(`http://localhost:5000/title/${title}`)
-        .then(response => {
-            console.log("Books by Title:");
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.log(error.message);
-        });
+async function getBookByTitle(title) {
+    const response = await axios.get(`${BASE_URL}/title/${title}`);
+    console.log(response.data);
 }
 
-// Example calls
 getAllBooks();
-getBookByISBN("1");
-getBooksByAuthor("Chinua Achebe");
-getBooksByTitle("Things Fall Apart");
+getBookByISBN(1);
+getBookByAuthor("Chinua Achebe");
+getBookByTitle("Things Fall Apart");
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,84 +1,39 @@
 const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
+const books = require('./booksdb.js');
 
+const general = express.Router();
 
-public_users.post("/register", (req, res) => {
-
-  const { username, password } = req.body;
-
-  if (!username || !password) {
-    return res.status(400).json({
-      message: "Username and password required"
-    });
-  }
-
-  if (!isValid(username)) {
-    return res.status(400).json({
-      message: "User already exists"
-    });
-  }
-
-  users.push({
-    username,
-    password
-  });
-
-  return res.status(200).json({
-    message: "User registered successfully"
-  });
-
+general.get('/', (req, res) => {
+  res.json(books);
 });
 
-// Get the book list available in the shop
-public_users.get('/', function (req, res) {
-  return res.status(200).json(books);
-});
+general.get('/isbn/:isbn', (req, res) => {
+  const book = books[req.params.isbn];
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-
-  if (book) {
-    return res.status(200).json(book);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
+  if (!book) {
+    return res.status(404).json({ message: 'Book not found' });
   }
+
+  return res.json(book);
 });
-  
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+
+general.get('/author/:author', (req, res) => {
   const author = req.params.author.toLowerCase();
-
-  const filteredBooks = Object.values(books).filter(book =>
-    book.author.toLowerCase() === author
+  const matchingBooks = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === author
   );
 
-  return res.status(200).json(filteredBooks);
+  return res.json(matchingBooks);
 });
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+
+general.get('/title/:title', (req, res) => {
   const title = req.params.title.toLowerCase();
-
-  const filteredBooks = Object.values(books).filter(book =>
-    book.title.toLowerCase().includes(title)
+  const matchingBooks = Object.values(books).filter(
+    (book) => book.title.toLowerCase() === title
   );
 
-  return res.status(200).json(filteredBooks);
-});
-// Get book review
-public_users.get('/review/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-
-  if (books[isbn]) {
-    return res.status(200).json(books[isbn].reviews);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
-  }
+  return res.json(matchingBooks);
 });
 
+module.exports.general = general;
 
-module.exports.general = public_users;
