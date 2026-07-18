cd88b9b6ea30e363f4fbddeb8e7b78e6715887e4
Nayab Naeem <nayabnaeem666@gmail.com>
2026-07-19 02:56:23 +0500

    Fix routers/general.js

--- final_project/general.js
+++ final_project/general.js
@@ -2,25 +2,51 @@ const axios = require("axios");
 
 const BASE_URL = "http://localhost:5000";
 
+// Task 10.1
 async function getAllBooks() {
-    const response = await axios.get(`${BASE_URL}/`);
-    console.log(response.data);
+    try {
+        const response = await axios.get(`${BASE_URL}/`);
+        console.log("All Books:");
+        console.log(response.data);
+    } catch (err) {
+        console.log(err.message);
+    }
 }
 
-async function getBookByISBN(isbn) {
-    const response = await axios.get(`${BASE_URL}/isbn/${isbn}`);
-    console.log(response.data);
+// Task 10.2
+function getBookByISBN(isbn) {
+    axios
+        .get(`${BASE_URL}/isbn/${isbn}`)
+        .then(response => {
+            console.log("Book by ISBN:");
+            console.log(response.data);
+        })
+        .catch(err => console.log(err.message));
 }
 
-async function getBookByAuthor(author) {
-    const response = await axios.get(`${BASE_URL}/author/${author}`);
-    console.log(response.data);
+// Task 10.3
+function getBookByAuthor(author) {
+    axios
+        .get(`${BASE_URL}/author/${author}`)
+        .then(response => {
+            console.log("Books by Author:");
+            console.log(response.data);
+        })
+        .catch(err => console.log(err.message));
 }
 
-async function getBookByTitle(title) {
-    const response = await axios.get(`${BASE_URL}/title/${title}`);
-    console.log(response.data);
+// Task 10.4
+function getBookByTitle(title) {
+    axios
+        .get(`${BASE_URL}/title/${title}`)
+        .then(response => {
+            console.log("Books by Title:");
+            console.log(response.data);
+        })
+        .catch(err => console.log(err.message));
 }
+
+// Call functions
 
 getAllBooks();
 getBookByISBN(1);
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const books = require('./booksdb.js');
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
 
 const general = express.Router();
+
+general.post("/register", (req, res) => {
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username and password required"
+        });
+    }
+
+    if (!isValid(username)) {
+        return res.status(400).json({
+            message: "User already exists"
+        });
+    }
+
+    users.push({
+        username,
+        password
+    });
+
+    return res.status(200).json({
+        message: "User registered successfully"
+    });
+
+});
 
 general.get('/', (req, res) => {
   res.json(books);
@@ -35,5 +64,19 @@ general.get('/title/:title', (req, res) => {
   return res.json(matchingBooks);
 });
 
+
+general.get('/review/:isbn', (req, res) => {
+
+    const isbn = req.params.isbn;
+
+    if (books[isbn]) {
+        return res.status(200).json(books[isbn].reviews);
+    }
+
+    return res.status(404).json({
+        message: "Book not found"
+    });
+
+});
 module.exports.general = general;
 
